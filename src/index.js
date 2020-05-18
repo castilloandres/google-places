73caab26433f70e0a3d30c61820624d13b0b42a0
myusername <castilloandres17@gmail.com>
2020-05-18 20:51:17 +0200

    Add text and nearby search

--- src/index.js
+++ src/index.js
@@ -1,51 +1,48 @@
 'use strict';
 
 const dotenv = require('dotenv');
-dotenv.config()
-const url = require('url');
-const GOOGLE_PLACES_ENDPOINT = process.env.GOOGLE_PLACES_ENDPOINT;
+dotenv.config();
+const TEXTSEARCH_ENDPOINT = process.env.TEXTSEARCH_ENDPOINT;
+const NEARBYSEARCH_ENDPOINT = process.env.NEARBYSEARCH_ENDPOINT;
 const GOOGLE_PLACES_API_KEY = process.env.GOOGLE_PLACES_API_KEY;
-console.log('GOOGLE_PLACES_API_KEY', GOOGLE_PLACES_API_KEY);
-
-const textSearchUrl = new URL(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+Paris+11&key=${GOOGLE_PLACES_API_KEY}`).href;
-console.log(textSearchUrl);
 const fetch = require('node-fetch');
+const fs = require('fs');
 
-const options = {
-	location: '',
-	radius: '',
-	types: '',
-	name: '',
-	key: ''
-};
+const query = 'restaurants+Paris+11';
+const location = '42.3675294,-71.186966';
+const radius = 1000;
 
-async function nearbySearch (options) {
-  // const url = concatenate url here
-  const result = await fetch(textSearchUrl);
+const textSearchParams = new URLSearchParams([
+  ['key', GOOGLE_PLACES_API_KEY],
+  ['query', query]
+]);
+
+const nearbySearchParams = new URLSearchParams([
+  ['key', GOOGLE_PLACES_API_KEY],
+  ['location', location],
+  ['radius', radius]
+]);
+
+const textSearchUrl = new URL(`${TEXTSEARCH_ENDPOINT}?${textSearchParams}`).href;
+const nearbySearchUrl = new URL(`${NEARBYSEARCH_ENDPOINT}?${nearbySearchParams}`).href;
+
+function nearbySearch () {
+  fetch(nearbySearchUrl)
+		.then(res => {
+		  const dest = fs.createWriteStream('./nearby-search-results.json');
+		  res.body.pipe(dest);
+		});
 }
 
-function textSearch (options) {
+function textSearch () {
   fetch(textSearchUrl)
 	 .then(res => {
-	    const dest = fs.createWriteStream('./results.json');
+	    const dest = fs.createWriteStream('./text-search-results.json');
 	    res.body.pipe(dest);
-	    console.log("call is done");
 	  });
 }
 
-
-// https://maps.googleapis.com/maps/api/place/nearbysearch/json?
-// location=-33.8670522,151.1957362
-// &radius=1500
-// &type=restaurant
-// &keyword=cruise
-// &key=YOUR_API_KEY
-
-// https://maps.googleapis.com/maps/api/place/textsearch/xml?
-// query=restaurants+in+Sydney&
-// key=YOUR_API_KEY
-
 module.exports = {
-  nearbySearch,
-  textSearch
-}
+  textSearch,
+  nearbySearch
+};
